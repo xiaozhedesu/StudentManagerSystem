d924784f9bdf640efb86ce57daf9fe6ff9f743af
xiaozhenano <xiaozhedesu0219@outlook.com>
2025-12-17 20:38:28 +0800

    修改：查找指令新增排序功能

--- src/main/java/cli/command/SaveCommand.java
+++ src/main/java/cli/command/SaveCommand.java
@@ -12,8 +12,10 @@ public class SaveCommand extends AbstractCommand implements Command{
 
     @Override
     public void execute() throws StudentSystemBusinessException {
+        System.out.println("正在存储数据...");
         try {
             studentService.saveStudentList();
+            System.out.println("数据存储成功。");
         } catch (IOException e) {
             // 写成这样是为了不改接口异常定义
             throw new StudentSystemBusinessException("发生IO错误：" + e.getMessage());
--- src/main/java/cli/command/SearchCommand.java
+++ src/main/java/cli/command/SearchCommand.java
@@ -4,8 +4,11 @@ import main.java.exception.InvalidValueException;
 import main.java.model.Student;
 import main.java.service.StudentService;
 
+import java.time.LocalDate;
 import java.util.Collection;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.stream.Stream;
 
 /**
  * 搜索指令
@@ -18,11 +21,14 @@ public class SearchCommand extends AbstractCommand implements Command {
     @Override
     public void execute() throws InvalidValueException {
         HashMap<String, Student> students = studentService.getStudentList();
+
+        // 无参
         if (params == NO_PARAM) {
             showStudentsInfo(students.values());
             return;
         }
 
+        // 参数不够2个
         if (params.length < 2) {
             throw new InvalidValueException("""
                     search指令需要2个参数：<fieldName> <value>
@@ -31,27 +37,37 @@ public class SearchCommand extends AbstractCommand implements Command {
                     另外，当search指令不传参数时，代表查询全部。""");
         }
 
-        String fieldName = params[0];
-        if ("id".equals(fieldName)) {
-            String id = params[1];
-            if (students.containsKey(id)) {
-                System.out.println(students.get(id));
-            } else {
-                System.out.println("未找到id为 " + id + " 的学生。");
-            }
-        } else if ("name".equals(fieldName)) {
-            String name = params[1];
-            Collection<Student> filtered_students = students.values().stream()
-                    .filter(student -> student.getName().equals(name))
-                    .toList();
-            showStudentsInfo(filtered_students);
-        } else {
-            throw new InvalidValueException("不支持的字段查询：" + fieldName);
+        // 不按字段查询的排序版
+        if ("sortBy".equals(params[0])) {
+            showStudentsInfo(students.values().stream()
+                    .sorted(parseComparator(params[1]))
+                    .toList());
+            return;
         }
+
+        // 按字段查询
+        String fieldName = params[0];
+        String value = params[1];
+        Stream<Student> stream = switch (fieldName) {
+            case "id" -> Stream.ofNullable(students.get(value));
+            case "name" -> students.values().stream()
+                    .filter(student -> student.getName().equals(value));
+            case "sex" -> students.values().stream()
+                    .filter(student -> student.getSex().equals(value));
+            default -> throw new InvalidValueException("不支持的字段查询：" + fieldName);
+        };
+
+        // 按字段查找的排序
+        if (params.length >= 4 && "sortBy".equals(params[2])) {
+            stream = stream.sorted(parseComparator(params[3]));
+        }
+
+        // 按字段查找的输出
+        showStudentsInfo(stream.toList());
     }
 
     /**
-     * 服用遍历展示学生信息方法
+     * 复用遍历展示学生信息方法
      *
      * @param students 学生列表
      */
@@ -64,5 +80,20 @@ public class SearchCommand extends AbstractCommand implements Command {
         for (Student student : students) {
             System.out.println(student);
         }
+    }
+
+    /**
+     * 提供给流排序选择排序规则
+     *
+     * @param key 字段名
+     * @return Comparator对象
+     */
+    private Comparator<Student> parseComparator(String key) {
+        return switch (key) {
+            case "name" -> Comparator.comparing(Student::getName);
+            case "sex" -> Comparator.comparing(Student::getSex);
+            case "birthday" -> Comparator.comparing(student -> LocalDate.parse(student.getBirthdayString()));
+            default -> Comparator.comparing(Student::getStudentId);
+        };
     }
 }
